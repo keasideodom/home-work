c10a3f0ba7e501e5139f22beb298a6970c871cdf
keasideodom <odmkc85@gmail.com>
2025-02-13 21:59:35 +0100

    helloword.js

---
+++ hyf-homework/A dog age calculator.js
@@ -0,0 +1,11 @@
+console.log('dog')
+let dogYearOfBirth = 2017;
+let dogYearFuture = 2027;
+let dogYear = 2017 - 2027;
+
+
+if (shouldShowResultInDogYears) {
+    console.log(`Your dog will be {2017 * 7} dog years old in {2027}`);
+} else {
+    console.log(`Your dog will {2017} human years old in {2027}`);
+}
---
+++ hyf-homework/Age-ify .js
@@ -0,0 +1,6 @@
+console.log('hello age');
+// Calculate the age
+let age = 2027 - 1985;
+
+// Output the result
+console.log(`In the year ${2045}, you will be ${60} years old.`);
